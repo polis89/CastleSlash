901ad406b57767f70dfdf4cfaed795c19e8d2918
polis89 <89polis@gmail.com>
2016-11-11 00:10:35 +0100

    Start Input Scripting

---
+++ Assets/GameInputController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInputController : MonoBehaviour {
+	private static string logTAG = "GameInputController";
+	private static ILogger logger = Debug.logger;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touches.Length != 0) {
+			logger.Log (logTAG, "Touches: " + Input.touches);
+		}
+		if(Input.GetMouseButton(0)){
+			//float x = Input.GetAxis ("Mouse X");
+			//float y = Input.GetAxis ("Mouse Y");
+			//logger.Log(logTAG, "Mouse X: " + x + "; Mouse Y: " + y);
+		}
+	}
+}
